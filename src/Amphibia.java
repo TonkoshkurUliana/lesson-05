b6af88e1d5d38dcda0b961c7f7135c719911064a
TonkoshkurUliana <118213848+TonkoshkurUliana@users.noreply.github.com>
2022-11-30 10:58:10 +0200

    Update Amphibia.java

--- src/Amphibia.java
+++ src/Amphibia.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Amphibia {
+public abstract class Amphibia {
+    void eat() {
+        System.out.println("Я їм");
+    }
+    void sleap(){
+        System.out.println("Я сплю");
+    }
+    void swim(){
+        System.out.println("Я плаваю");
+    }
+    void walk (){
+        System.out.println("Я гуляю");
+    }
 }
